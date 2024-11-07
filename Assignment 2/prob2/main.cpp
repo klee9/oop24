ad2ec1c3ac074c84804e9f0d2a1ecf8a532c6f27
Keon Lee <22269190+klee9@users.noreply.github.com>
2024-11-07 11:21:24 +0900

    Update main.cpp

--- Assignment 2/prob2/main.cpp
+++ Assignment 2/prob2/main.cpp
@@ -11,11 +11,9 @@ int main()
 {
     string input, left_op, right_op, op;
     
-    cout << "A simple calculator for infinite integer. Enter 0 to exit." << endl;
-    cout << "Usage: (integer)(space)(operator)(space)(integer)" << endl;
-    
     while(true) {
         // get input
+        cout << "Input: ";
         getline(cin, input);
         if(input == "0") return 0;
 
