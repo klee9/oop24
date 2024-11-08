48e08985c620d41b686d344a9e9bca2d4bad183a
Keon Lee <22269190+klee9@users.noreply.github.com>
2024-11-08 12:16:13 +0900

    Update main.cpp

--- Assignment 2/prob1/main.cpp
+++ Assignment 2/prob1/main.cpp
@@ -6,7 +6,7 @@ using namespace std;
 
 int main() {
     // testing constructors
-    cout << "Testing constructors...\n";
+    cout << "[Constructors]\n";
     
     inf_int a;                    
     inf_int b(123456789);
@@ -21,13 +21,13 @@ int main() {
     cout << "Copy constructor (e = b): " << e << " (Expected: 123456789)\n";
 
     // testing comparison
-    cout << "\nTesting comparison operators...";
+    cout << "\n\n[Comparison Operators]";
     inf_int f("12345"), g("54321"), h("12345"), i("-12345");
 
     cout << "\nf = " << f;
     cout << "\ng = " << g; 
     cout << "\nh = " << h;
-    cout << "\ni = " << i;
+    cout << "\ni = " << i << "\n\n";
     
     cout << "f == h: " << (f == h) << " (Expected: 1)\n";
     cout << "f != g: " << (f != g) << " (Expected: 1)\n";
@@ -37,61 +37,63 @@ int main() {
     cout << "i == i: " << (i == i) << " (Expected: 1)\n";
 
     // testing addition
-    cout << "\nTesting addition...";
-    inf_int pos1("12345678901234567890"), pos2("98765432109876543210");
-    inf_int neg1("-12345678901234567890"), neg2("-98765432109876543210");
+    cout << "\n\n[Addition]";
+    inf_int pos_add1("12345678901234567890"), pos_add2("98765432109876543210");
+    inf_int neg_add1("-12345678901234567890"), neg_add2("-98765432109876543210");
 
-    cout << "\npos1: " << pos1;
-    cout << "\npos2: " << pos2;
-    cout << "\nneg1: " << neg1;
-    cout << "\nneg2: " << neg2;
+    cout << "\npos_add1: " << pos_add1;
+    cout << "\npos_add2: " << pos_add2;
+    cout << "\nneg_add1: " << neg_add1;
+    cout << "\nneg_add2: " << neg_add2 << "\n\n";
 
-    cout << "pos1 + pos2 = " << (pos1 + pos2) << " (Expected: 111111111011111111100)\n";
-    cout << "pos1 + neg1 = " << (pos1 + neg1) << " (Expected: 0)\n";
-    cout << "pos1 + neg2 = " << (pos1 + neg2) << " (Expected: -86419753208641975320)\n";
-    cout << "neg1 + neg2 = " << (neg1 + neg2) << " (Expected: -111111111011111111100)\n";
+    cout << "pos_add1 + pos_add2 = " << (pos_add1 + pos_add2) << " (Expected: 111111111011111111100)\n";
+    cout << "pos_add1 + neg_add1 = " << (pos_add1 + neg_add1) << " (Expected: 0)\n";
+    cout << "pos_add1 + neg_add2 = " << (pos_add1 + neg_add2) << " (Expected: -86419753208641975320)\n";
+    cout << "neg_add1 + neg_add2 = " << (neg_add1 + neg_add2) << " (Expected: -111111111011111111100)\n";
 
     // testing subtraction
-    cout << "\nTesting subtraction...\n";
+    cout << "\n\n[Subtraction]";
 
-    cout << "\npos1: " << pos1;
-    cout << "\npos2: " << pos2;
-    cout << "\nneg1: " << neg1;
-    cout << "\nneg2: " << neg2;
+    cout << "\npos_add1: " << pos_add1;
+    cout << "\npos_add2: " << pos_add2;
+    cout << "\nneg_add1: " << neg_add1;
+    cout << "\nneg_add2: " << neg_add2 << "\n\n";
     
-    cout << "pos1 - pos1 = " << (pos1 - pos1) << " (Expected: 0)\n";
-    cout << "pos2 - pos1 = " << (pos2 - pos1) << " (Expected: 86419753208641975320)\n";
-    cout << "pos1 - neg1 = " << (pos1 - neg1) << " (Expected: 24691357802469135780)\n";
-    cout << "neg1 - pos1 = " << (neg1 - pos1) << " (Expected: -24691357802469135780)\n";
-    cout << "neg1 - neg1 = " << (neg1 - neg1) << " (Expected: 0)\n";
-    cout << "neg2 - neg1 = " << (neg2 - neg1) << " (Expected: -86419753208641975320)\n";
+    cout << "pos_add1 - pos_add1 = " << (pos_add1 - pos_add1) << " (Expected: 0)\n";
+    cout << "pos_add2 - pos_add1 = " << (pos_add2 - pos_add1) << " (Expected: 86419753208641975320)\n";
+    cout << "pos_add1 - neg_add1 = " << (pos_add1 - neg_add1) << " (Expected: 24691357802469135780)\n";
+    cout << "neg_add1 - pos_add1 = " << (neg_add1 - pos_add1) << " (Expected: -24691357802469135780)\n";
+    cout << "neg_add1 - neg_add1 = " << (neg_add1 - neg_add1) << " (Expected: 0)\n";
+    cout << "neg_add2 - neg_add1 = " << (neg_add2 - neg_add1) << " (Expected: -86419753208641975320)\n";
 
     // testing multiplication
-    cout << "\nTesting multiplication...\n";
-    inf_int pos("123456"), neg("-654321");
+    cout << "\n\n[Multiplication]";
+    inf_int mul_pos("123456"), mul_neg("-654321");
 
-    cout << "\npos: " << pos;
-    cout << "\nneg: " << neg;
+    cout << "\nmul_pos: " << mul_pos;
+    cout << "\nmul_neg: " << mul_neg << "\n\n";
     
-    cout << "pos * pos = " << (mul_pos * mul_pos) << " (Expected: 15241383936)\n";
-    cout << "pos * neg = " << (mul_pos * mul_neg) << " (Expected: -80779853376)\n";
-    cout << "neg * pos = " << (mul_neg * mul_pos) << " (Expected: -80779853376)\n";
-    cout << "neg * neg = " << (mul_neg * mul_neg) << " (Expected: 428107856641)\n";
+    cout << "mul_pos * mul_pos = " << (mul_pos * mul_pos) << " (Expected: 15241383936)\n";
+    cout << "mul_pos * mul_neg = " << (mul_pos * mul_neg) << " (Expected: -80779853376)\n";
+    cout << "mul_neg * mul_pos = " << (mul_neg * mul_pos) << " (Expected: -80779853376)\n";
+    cout << "mul_neg * mul_neg = " << (mul_neg * mul_neg) << " (Expected: 428107856641)\n";
 
     // testing division
-    cout << "\nTesting division...\n";
-    inf_int pos("121932631112635269"), neg("-121932631112635269");
+    cout << "\n\n[Division]";
+    inf_int div_pos("121932631112635269"), div_neg("-121932631112635269");
 
-    cout << "\npos: " << pos;
-    cout << "\nneg: " << neg;
+    cout << "\nmul_pos: " << mul_pos;
+    cout << "\ndiv_pos: " << div_pos;
+    cout << "\nmul_neg: " << mul_neg;
+    cout << "\ndiv_neg: " << div_neg << "\n\n";
     
-    cout << "pos / pos = " << (div_pos / mul_pos) << " (Expected: 987660633040.397137...)\n";
-    cout << "pos / neg = " << (div_pos / mul_neg) << " (Expected: -186349866674.973398...)\n";
-    cout << "neg / pos = " << (div_neg / mul_pos) << " (Expected: -987660633040.397137...)\n";
-    cout << "neg / neg = " << (div_neg / mul_neg) << " (Expected: 186349866674.973398...)\n";
+    cout << "div_pos / mul_pos = " << (div_pos / mul_pos) << " (Expected: 987660633040.397137...)\n";
+    cout << "div_pos / mul_neg = " << (div_pos / mul_neg) << " (Expected: -186349866674.973398...)\n";
+    cout << "div_neg / mul_pos = " << (div_neg / mul_pos) << " (Expected: -987660633040.397137...)\n";
+    cout << "div_neg / mul_neg = " << (div_neg / mul_neg) << " (Expected: 186349866674.973398...)\n";
 
     // testing edge cases
-    cout << "\nTesting edge cases with 0...\n";
+    cout << "\n\n[Edge Cases with 0]\n";
     inf_int zero("0"), one("1"), neg_one("-1");
 
     cout << "0 + 0: " << (zero + zero) << " (Expected: 0)\n";
@@ -100,7 +102,7 @@ int main() {
     cout << "1 / 1: " << (one / one) << " (Expected: 1)\n";
 
     // testing I/O operators
-    cout << "\nTesting I/O operators...\n";
+    cout << "\n\n[I/O]\n";
     inf_int input_test;
     cout << "Enter a large integer: ";
     cin >> input_test;
